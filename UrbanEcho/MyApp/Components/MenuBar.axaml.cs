b3a14062bde1985fb1003c2f97ccf1c381a9a06f
Michael Lemelin <Mlemelin1097@conestogac.on.ca>
2026-02-03 00:02:01 -0500

    Created menu bar component

---
+++ UrbanEcho/MyApp/Components/MenuBar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyApp;
+
+public partial class MenuBar : UserControl
+{
+    public MenuBar()
+    {
+        InitializeComponent();
+    }
+}
