b037dd9a1cd2048de9eae9a0e1cce65a0d7f5d78
Michael Lemelin <Mlemelin1097@conestogac.on.ca>
2026-02-03 17:43:11 -0500

    Created footer component and fixed issue with title bar being transparent

---
+++ UrbanEcho/MyApp/Components/FooterBar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyApp;
+
+public partial class FooterBar : UserControl
+{
+    public FooterBar()
+    {
+        InitializeComponent();
+    }
+}
