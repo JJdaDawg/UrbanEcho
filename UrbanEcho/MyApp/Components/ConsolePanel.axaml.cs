88413e50bc8fd97aceb55a62236c8066a12a038a
Michael Lemelin <Mlemelin1097@conestogac.on.ca>
2026-02-04 23:30:33 -0500

    Created Console panel component. Fixed spacing issues.

---
+++ UrbanEcho/MyApp/Components/ConsolePanel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyApp;
+
+public partial class ConsolePanel : UserControl
+{
+    public ConsolePanel()
+    {
+        InitializeComponent();
+    }
+}
