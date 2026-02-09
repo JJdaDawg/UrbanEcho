ed989cc2bf398ed91b98fd60a9c7b67329f1fff0
TinoRestivo <145874937+TinoRestivo@users.noreply.github.com>
2026-02-09 14:41:24 -0500

    Added NUnit test project

---
+++ UrbanEcho/UrbanEcho.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿namespace UrbanEcho.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        ProjectFile? projectFile = null;
+        try
+        {
+            ProjectFile.Open("testfile");
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail($"Project file didn't open with error {ex.ToString()}");
+        }
+
+        if (projectFile == null)
+        {
+            Assert.Fail($"Project file null");
+        }
+    }
+}
