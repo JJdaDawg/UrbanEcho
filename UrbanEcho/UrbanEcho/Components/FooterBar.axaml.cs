5e7179c2eea11653e865829692bbdd6b40879be4
Michael Lemelin <Mlemelin1097@conestogac.on.ca>
2026-02-13 17:21:58 -0500

    Added code to update the footer background to change colour indicating if the simulation is running or not.

--- UrbanEcho/UrbanEcho/Components/FooterBar.axaml.cs
+++ UrbanEcho/UrbanEcho/Components/FooterBar.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using UrbanEcho.ViewModels;
 
 namespace UrbanEcho;
 
@@ -9,5 +11,17 @@ public partial class FooterBar : UserControl
     public FooterBar()
     {
         InitializeComponent();
+
+        this.DataContextChanged += (s, e) =>
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Simulation.PropertyChanged += (_, __) =>
+                {
+                    // Update background when running state changes
+                    FooterBorder.Background = vm.Simulation.IsRunning ? Brushes.IndianRed : Brushes.Black;
+                };
+            }
+        };
     }
 }
