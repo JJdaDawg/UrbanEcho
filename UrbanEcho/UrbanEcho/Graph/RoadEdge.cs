cd746243c5514f01ec3c5542f7f9101ca7ab1138
Joshua Jones <jrj2004@hotmail.ca>
2026-02-17 17:17:54 -0500

    Added Graph loading logic into main project

---
+++ UrbanEcho/UrbanEcho/Graph/RoadEdge.cs
@@ -0,0 +1,18 @@
+﻿public sealed class RoadEdge
+{
+    public int From { get; }
+    public int To { get; }
+    public double Length { get; }
+    public RoadMetadata Metadata { get; }
+
+    public RoadEdge(int from, int to, double length, RoadMetadata metadata)
+    {
+        From = from;
+        To = to;
+        Length = length;
+        Metadata = metadata;
+    }
+
+    public double TravelTimeSeconds =>
+        Length / Metadata.SpeedLimit;
+}
---
+++ UrbanEcho/UrbanEcho/Graph/RoadNode.cs
@@ -0,0 +1,13 @@
+﻿public sealed class RoadNode
+{
+    public int Id { get; }
+    public double X { get; }
+    public double Y { get; }
+
+    public RoadNode(int id, double x, double y)
+    {
+        Id = id;
+        X = x;
+        Y = y;
+    }
+}
