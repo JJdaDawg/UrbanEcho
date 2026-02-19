764b24babe322163e758579f7d0f8891063a107a
Joshua Jones <jrj2004@hotmail.ca>
2026-02-18 22:22:50 -0500

    added a* implementation and started trying to get cars to follow the path. can't yet do it properly yet

--- UrbanEcho/UrbanEcho/Graph/RoadGraph.cs
+++ UrbanEcho/UrbanEcho/Graph/RoadGraph.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public sealed class RoadGraph
 {
     public IReadOnlyDictionary<int, RoadNode> Nodes { get; }
     public IReadOnlyList<RoadEdge> Edges { get; }
+
+    private readonly Dictionary<int, List<RoadEdge>> _adjacency;
 
     public RoadGraph(
         Dictionary<int, RoadNode> nodes,
@@ -11,5 +14,25 @@ public sealed class RoadGraph
     {
         Nodes = nodes;
         Edges = edges;
+
+        _adjacency = new Dictionary<int, List<RoadEdge>>();
+
+        foreach (var edge in edges)
+        {
+            if (!_adjacency.TryGetValue(edge.From, out var list))
+            {
+                list = new List<RoadEdge>();
+                _adjacency[edge.From] = list;
+            }
+
+            list.Add(edge);
+        }
+    }
+
+    public IReadOnlyList<RoadEdge> GetOutgoingEdges(int nodeId)
+    {
+        return _adjacency.TryGetValue(nodeId, out var edges)
+            ? edges
+            : Array.Empty<RoadEdge>();
     }
 }
