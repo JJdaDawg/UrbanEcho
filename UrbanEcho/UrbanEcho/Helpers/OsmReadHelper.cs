30d33b3dc510d056f6f2bded2216577773ca720f
TinoRestivo <145874937+TinoRestivo@users.noreply.github.com>
2026-03-24 13:46:09 -0400

    Made change so if osm data has two stop signs close together it is treated as one stop sign using average position. Made change to Sim so it spawns less cars on simpler network

--- UrbanEcho/UrbanEcho/Helpers/OsmReadHelper.cs
+++ UrbanEcho/UrbanEcho/Helpers/OsmReadHelper.cs
@@ -3,6 +3,7 @@ using BruTile;
 using DocumentFormat.OpenXml.Bibliography;
 using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
 using FluentAvalonia.UI.Media;
 using Mapsui;
 using Mapsui.Nts;
@@ -28,6 +29,9 @@ namespace UrbanEcho.Helpers
 {
     public static class OsmReadHelper
     {
+        private static List<string> AllowableRoadTypes = PopulateAllowableRoadTypes();
+        private static List<string> AllowableIntersectionTypes = PopulateAllowableIntersectionTypes();
+
         public static List<IFeature> GetRoadFeatures(string path)
         {
             //part of this code from here
@@ -568,11 +572,100 @@ namespace UrbanEcho.Helpers
                 EventQueueForUI.Instance.Add(new LogToConsole(MainWindow.Instance.GetMainViewModel(), $"Failed to get features from {path} - {ex.Message}"));
             }
 
+            featuresList = JoinStopIntersectionsCloseBy(featuresList);
+
             return featuresList;
         }
 
-        private static List<string> AllowableRoadTypes = PopulateAllowableRoadTypes();
-        private static List<string> AllowableIntersectionTypes = PopulateAllowableIntersectionTypes();
+        public static List<IFeature> JoinStopIntersectionsCloseBy(List<IFeature> featuresList)
+        {
+            List<IFeature> newFeatures = new List<IFeature>();
+
+            List<IFeature> toRemoveFeatures = new List<IFeature>();
+
+            double tolerance = 40.0f;
+
+            foreach (IFeature feature in featuresList)
+            {
+                List<IFeature> thisPassRemoveFeatures = new List<IFeature>();
+                bool joinTheFeature = false;
+
+                if (Helper.TryGetFeatureKVPToString(feature, "Intersec_1", "") == "All Way Stop")
+                {
+                    if (!toRemoveFeatures.Contains(feature))
+                    {
+                        foreach (IFeature otherFeature in featuresList)
+                        {
+                            if (Helper.TryGetFeatureKVPToString(otherFeature, "Intersec_1", "") == "All Way Stop")
+                            {
+                                if (feature != otherFeature && !toRemoveFeatures.Contains(otherFeature))
+                                {
+                                    if (feature is GeometryFeature gf1 && gf1.Geometry is Point p1 && otherFeature is GeometryFeature gf2 && gf2.Geometry is Point p2)
+                                    {
+                                        double distance = p1.Distance(p2);
+                                        EventQueueForUI.Instance.Add(new LogToConsole(MainWindow.Instance.GetMainViewModel(), $"stop distance {distance}"));
+
+                                        if (p1.Coordinate.Equals2D(p2.Coordinate, tolerance))
+                                        {
+                                            joinTheFeature = true;
+
+                                            if (!thisPassRemoveFeatures.Contains(feature))
+                                            {
+                                                thisPassRemoveFeatures.Add(feature);
+                                            }
+
+                                            if (!thisPassRemoveFeatures.Contains(otherFeature))
+                                            {
+                                                thisPassRemoveFeatures.Add(otherFeature);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (joinTheFeature)
+                {
+                    double averagePosX = 0;
+                    double averagePosY = 0;
+
+                    foreach (IFeature addToRemoveFeature in thisPassRemoveFeatures)
+                    {
+                        if (addToRemoveFeature is GeometryFeature gf1 && gf1.Geometry is Point p1)
+                        {
+                            averagePosX = averagePosX + p1.X / (double)(thisPassRemoveFeatures.Count);
+                            averagePosY = averagePosY + p1.Y / (double)(thisPassRemoveFeatures.Count);
+
+                            if (!toRemoveFeatures.Contains(addToRemoveFeature))
+                                toRemoveFeatures.Add(addToRemoveFeature);
+                        }
+                    }
+
+                    if (feature is GeometryFeature gfOld)
+                    {
+                        Point newPoint = new Point(averagePosX, averagePosY);
+                        GeometryFeature gf = new GeometryFeature(newPoint);
+
+                        gf["Intersecti"] = gfOld["Intersecti"];
+                        gf["Intersec_1"] = gfOld["Intersec_1"];
+                        gf["highway"] = gfOld["highway"];
+
+                        newFeatures.Add(gf);
+                    }
+                }
+                else
+                {
+                    if (!toRemoveFeatures.Contains(feature))
+                    {
+                        newFeatures.Add(feature);
+                    }
+                }
+            }
+
+            return newFeatures;
+        }
 
         public static bool CheckRoadTypeAllowed(string stringToCheck)
         {
